66c0be7259d3ce84e8af01b94c1aaaddff258023
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-01-31 16:56:16 +0530

    doc status updated

--- TempNotes/src/App.jsx
+++ TempNotes/src/App.jsx
@@ -5,11 +5,32 @@ import Card from './compnents/card'
 import Nav from './compnents/nav'
 
 function App() {
+
+  const data = [
+    {
+        Title:"Task 1",
+        desc:"This is my frist notes where i am storing my all upcoming tasks  ",
+        status : "pending",
+        tagColor : "red"
+    }, 
+    {
+      Title:"Task 2",
+      desc:"Php is the server side scripting language i want to learn php with laravel and i great at react js ",
+      
+      status : "Solved",
+      tagColor : "green"
+    }
+    ];
+
   return (
     <>
       <div className="fg">
       <Nav/>
-      <Card/>
+      <div className="cardDis flex gap-10">
+          {data.map((item,index) => (
+            <Card data={item}/>
+          ))}
+      </div>
       </div>
       <div className="bg">
          <h1 className='font-bold'>TempNotes</h1>
--- TempNotes/src/compnents/card.jsx
+++ TempNotes/src/compnents/card.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({data}) => {
+    
+    
   return (
    <div className="cards">
     <i class="ri-close-circle-fill close cursor-pointer"></i>
-    <div className="whitefill"></div>
     <div className="card max-w-[16vw] h-[20vw] bg-slate-200 rounded-[1vw] ">
        <div className="topcardicons">
        <i class="ri-article-fill docicon"></i> 
-        <h3> Task 1</h3>    
+        <h3> {data.Title}</h3>    
        </div>
         <p>
-            this is my first note here i am
-            uploading my tasks his is my first note here i am
-            uploading my tasksh is is my first note here i am
-            uploading my taskshis is my first note here i am
-            uploading my taskshis is my first note here i am
-            uploading my taskshis is my 
+           {data.desc}
         </p>
 
-        <div className="cardStatus">
-            <h3>Pending</h3>
+        <div className={`cardStatus ${data.tagColor === "green" ? "bg-green-700" : "bg-red-600"}`}>
+            <h3>{data.status}</h3>
         </div>
     </div>
    </div>
